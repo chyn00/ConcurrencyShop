a878264322aaf64c6df3841c2c6ed5a0a0c01771
mynameiskuun <mynameiskuun@gmail.com>
2024-03-11 23:59:51 +0900

    feat. 주문 후 재고 차감 테스트

--- src/main/java/com/shop/concurrency/common/exception/filter/constant/ExceptionCodeEnum.java
+++ src/main/java/com/shop/concurrency/common/exception/filter/constant/ExceptionCodeEnum.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCodeEnum {
 
-    DuplicatedMemberException("중복된 회원입니다. 다시 확인해주세요.", HttpStatus.CONFLICT);
+    DuplicatedMemberException("중복된 회원입니다. 다시 확인해주세요.", HttpStatus.CONFLICT),
+    OverstockDeductionException("재고보다 많은 양을 주문하셨습니다. 다시 주문 해 주세요.", HttpStatus.BAD_REQUEST);
     private String message;
     private HttpStatus httpStatus;
 
